8e2bce8ff00894b884eceb622b3cbf6f297dc6ef
Sara Brandes <smbrandes17@gmail.com>
2018-01-06 00:10:08 -0800

    solved JSONstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+    var result = '';
+
+    if (obj === undefined) {
+
+    }
+
+    else if (typeof obj === 'function') {
+
+    }
+
+    else if( Array.isArray(obj) ) {
+      result += '[';
+      for (var i = 0; i < obj.length; i++) {
+        result += stringifyJSON(obj[i]);
+        if( i !== obj.length - 1) {
+          result += ',';
+        }
+      }
+      result += ']';
+    } else if( typeof obj === 'object' ) {
+      if (obj === null) {
+        result += 'null';
+      }
+      else {
+        var keys = Object.keys(obj);
+        result += '{';
+        keys.forEach( function (key) {
+          if (typeof obj[key] === 'function' || obj[key] === undefined ) {
+
+          } else {
+            result += '"' + key + '":';
+            result += stringifyJSON(obj[key]);
+            if (key !== keys[keys.length - 1]) {
+              result += ',';
+            }
+          }
+        });
+       result += '}';
+      }
+    } else if (typeof obj === 'string') {
+      result += '"';
+      result += obj;
+      result += '"';
+    } else {
+      result += obj;
+    }
+    return result;
 };
